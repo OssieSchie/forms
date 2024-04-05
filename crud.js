d2e79aa9f8337c5765e91ba40dc94c59370a252b
OssieSchie <osvald.rye@gmail.com>
2024-04-05 19:45:27 +0200

    FORMålsløs form haha...

--- crud.js
+++ crud.js
@@ -20,17 +20,61 @@ export async function methodGet(){
 }
 
 export async function methodDelete(id){
+  let headersList = {
+    Accept: "application/json",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbXZoeGhocHhqeWxzZG9tdm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE1MDksImV4cCI6MjAyNjMyNzUwOX0.I2LR0bG62kXNTWyOhEaALU7cOyHKgtmiwBfaO5cYxY0",
+    Prefer: "return=representation",
+  };
 
+  let response = await fetch("https://aomvhxhhpxjylsdomvmt.supabase.co/rest/v1/recipes?id=eq." + id, {
+    method: "DELETE",
+    headers: headersList,
+  });
+
+  let data = await response.json();
 
   return data;
 }
 
-export async function methodPatch(){
+export async function methodPatch( id, state = true){
+  let headersList = {
+    Accept: "application/json",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbXZoeGhocHhqeWxzZG9tdm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE1MDksImV4cCI6MjAyNjMyNzUwOX0.I2LR0bG62kXNTWyOhEaALU7cOyHKgtmiwBfaO5cYxY0",
+    Prefer: "return=representation",
+    "Content-Type": "application/json",
+  };
 
+  let bodyContent = JSON.stringify({
+    studentFriendly: state,
+  });
 
-  // "studentFriendly" = false
+  let response = await fetch("https://aomvhxhhpxjylsdomvmt.supabase.co/rest/v1/recipes?id=eq." + id, {
+    method: "PATCH",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+
+  return data;
 }
 
-export async function methodSubmit(){
+export async function methodSubmit(recipe){
+  let headersList = {
+    Accept: "application/json",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbXZoeGhocHhqeWxzZG9tdm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE1MDksImV4cCI6MjAyNjMyNzUwOX0.I2LR0bG62kXNTWyOhEaALU7cOyHKgtmiwBfaO5cYxY0",
+    Prefer: "return=representation",
+    "Content-Type": "application/json",
+  };
 
+  let bodyContent = JSON.stringify(recipe);
+
+  let response = await fetch("https://aomvhxhhpxjylsdomvmt.supabase.co/rest/v1/recipes", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.text();
+  console.log(data);
 }
--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-import methodGet from "crud.js";
+import {methodGet, methodDelete, methodPatch, methodSubmit} from "./crud.js";
+
+showRecipes()
 
 async function showRecipes(){
     const response = await methodGet();
     const el = document.querySelector("template").content;
     const parent = document.querySelector(".recipes");
     parent.innerHTML = "";
-    const clone = el.cloneNode(true);
     response.forEach(rec=>{
+        const clone = el.cloneNode(true);
         clone.querySelector("[data-name]").textContent = rec.name;
         clone.querySelector("[data-origin]").textContent = rec.origin;
+        clone.querySelector("[data-diet]").textContent = rec.diet;
+        clone.querySelector("[data-servings]").textContent = rec.servings;
         if(rec.studentFriendly){
             clone.querySelector(".status").hidden = false;
 
         } else {
             clone.querySelector(".status").hidden = true;
         }
+
         clone.querySelectorAll("[data-id").forEach((e) => (e.dataset.id = rec.id));
-        clone.querySelector("button{data-action='delete'").addEventListener("click", async ()=>{
+        clone.querySelector("button[data-action='delete']").addEventListener("click", async ()=>{
            await methodDelete(rec.id);
+           console.log("deleted id = " + rec.id);
            await showRecipes();
         })
-        clone.querySelector("button{data-action='update'").addEventListener("click", async ()=>{
+        clone.querySelector("button[data-action='update']").addEventListener("click", async ()=>{
            await methodPatch(rec.id, !rec.studentFriendly);
+           console.log("updated id = " + rec.id);
            await showRecipes();
         })
         parent.appendChild(clone);
@@ -30,18 +37,23 @@ async function showRecipes(){
 
 function handleSubmit(){
     const form = document.querySelector("form");
-    form.addEventListener("submit", async e=>{
+    form.addEventListener("submit", async (e)=>{
         console.log(e);
-        // stop refresh
+        // vvv  stops refresh
         e.preventDefault();
+
         const formData = new FormData(form);
         console.log(formData.get("ingredients").split("\n"));
 
-        await createRecipe({
+        await methodSubmit({
             name: formData.get("name"),
+            description: formData.get("description"),
             ingredients: formData.get("ingredients").split("\n"),
-            allergens: formData.get("allergens").split("\n")
-            // ---
+            allergens: formData.get("allergens").split("\n"),
+            servings: formData.get("servings"),
+            diet: formData.get("diet"),
+            studentFriendly: formData.get("studentFriendly"),
+            origin: formData.get("origin"),
         })
 
         showRecipes();
